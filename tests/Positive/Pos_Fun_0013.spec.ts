32da78d2937d9b148ce4b23dcfae9f11e99814a5
Himal Nethmina <himal.nethmina2003@gmail.com>
2026-01-30 22:19:17 +0530

    numbers changed

--- tests/Positive/Pos_Fun_0013.spec.ts
+++ tests/Positive/Pos_Fun_0013.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('Pos_Fun_0001 - Simple greeting conversion', async ({ page }) => {
+test('Pos_Fun_0013 - Simple greeting conversion', async ({ page }) => {
   await page.goto('https://www.swifttranslator.com/');
 
   // Input
--- tests/Positive/Pos_Fun_0014.spec.ts
+++ tests/Positive/Pos_Fun_0014.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('Pos_Fun_0001 - Simple greeting conversion', async ({ page }) => {
+test('Pos_Fun_0014 - Simple greeting conversion', async ({ page }) => {
   await page.goto('https://www.swifttranslator.com/');
 
   // Input
