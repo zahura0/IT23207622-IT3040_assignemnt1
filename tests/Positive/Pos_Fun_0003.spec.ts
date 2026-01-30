78c8dc55a387516fbd366ee2cfeaeeaab732e525
Himal Nethmina <himal.nethmina2003@gmail.com>
2026-01-30 22:09:43 +0530

    pos 003

--- tests/Positive/Pos_Fun_0003.spec.ts
+++ tests/Positive/Pos_Fun_0003.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('Pos_Fun_0002 - Simple need statement', async ({ page }) => {
+test('Pos_Fun_0003 - Compound sentence with contrast', async ({ page }) => {
   await page.goto('https://www.swifttranslator.com/');
 
   // Input
@@ -9,10 +9,10 @@ test('Pos_Fun_0002 - Simple need statement', async ({ page }) => {
   });
 
   // Enter Singlish
-  await singlishInput.fill('mata kaema oonee.');
+  await singlishInput.fill('mama enavaa, namuth eyaa inne naehae.');
 
   // Wait until Sinhala text appears
-  const sinhalaOutput = page.getByText('මට කැම ඕනේ.');
+  const sinhalaOutput = page.getByText('මම එනවා, නමුත් එයා ඉන්නේ නැහැ.');
 
   await expect(sinhalaOutput).toBeVisible();
 });
