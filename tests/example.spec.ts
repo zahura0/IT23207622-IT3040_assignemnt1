de8fd6f695b92f048ee0d6b12d114f04ef4f4a7c
Himal Nethmina <himal.nethmina2003@gmail.com>
2026-01-30 20:42:44 +0530

    all working

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -16,3 +16,5 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+
