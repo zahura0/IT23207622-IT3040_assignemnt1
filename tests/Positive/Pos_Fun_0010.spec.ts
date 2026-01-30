75b83c8ed4300192981674ffbbc4f23f85d17a8f
Himal Nethmina <himal.nethmina2003@gmail.com>
2026-01-30 22:58:26 +0530

    10 working

--- tests/Positive/Pos_Fun_0010.spec.ts
+++ tests/Positive/Pos_Fun_0010.spec.ts
@@ -9,10 +9,10 @@ test('Pos_Fun_0010 - Time reference', async ({ page }) => {
   });
 
   // Enter Singlish
-  await singlishInput.fill('mama vaeda karanavaa.');
+  await singlishInput.fill('meeting eka 9.15 AM.');
 
   // Wait until Sinhala text appears
-  const sinhalaOutput = page.getByText('මම වැඩ කරනවා.');
+  const sinhalaOutput = page.getByText('meeting එක 9.15 AM.');
 
   await expect(sinhalaOutput).toBeVisible();
 });
